9c0209048bfebe8ee136df3564da22e022bd51b4
lichadev <licha.pintos@gmail.com>
2024-02-26 00:18:11 -0300

    solve bugs

--- lambda.tf
+++ lambda.tf
@@ -8,12 +8,11 @@ resource "aws_kms_alias" "lambda" {
   target_key_id = aws_kms_key.a.key_id
 }
 
-resource "aws_lambda_permission" "cloudwatch_call_lambda" {
+resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.lambda_function.arn
 }
 
 resource "aws_lambda_function" "lambda_function" {
