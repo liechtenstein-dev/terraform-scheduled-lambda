25f160b5293d3dc0c588c312a42931400045a906
lichadev <licha.pintos@gmail.com>
2024-02-25 23:57:57 -0300

    using package

--- variables.tf
+++ variables.tf
@@ -3,10 +3,9 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "state" {
-  description = "The state of the CloudWatch Event Rule"
-  type = string
-  default     = "ENABLED"
+variable "distribution_pkg_folder" {
+  description = "Folder name to create distribution files..."
+  default = "./lambda_dist_pkg"
 }
 
 variable "source_dir" {
@@ -37,23 +36,12 @@ variable "lambda_runtime_handler" {
   default     = "python3.8"
 }
 
-variable "retention_in_days" {
-  description = "Number of days to retain logs in CloudWatch"
-  default     = 14
-}
-
-variable "lambda_environment_variables" {
-  description = "Environment variables for the Lambda function"
-  type        = map(string)
-  default     = null
-}
-
-variable "schedule_expression" {
-  description = "The schedule expression for the CloudWatch Event Rule"
-  default     = "rate(1 day)"
-}
-
 variable "lambda_function_handler" {
   description = "The handler for the Lambda function, in the form of 'filename.handler', e.g. 'index.handler' for index.js file with handler function 'handler'"
   default     = "index.handler"
+}
+
+variable "output_path" {
+  description = "The path to the output file"
+  default     = "/output/lambda.zip"
 }
