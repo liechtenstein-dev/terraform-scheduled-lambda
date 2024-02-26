ac79a76cf6b54e9bcc9c5ff614b57e4301392281
Lisandro Pintos <64507701+lichadev@users.noreply.github.com>
2024-02-26 03:36:30 +0000

    bugs

--- policies.tf
+++ policies.tf
@@ -4,30 +4,41 @@ resource "aws_iam_policy" "lambda_policy" {
 
   policy = jsonencode(
     {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": [
-          "kms:ListAliases",
-          "kms:Decrypt"
-        ],
+      "Version": "2012-10-17",
+      "Statement": [
+    {
+      "Action": [
+        "kms:ListAliases",
+        "kms:Decrypt"
+      ],
         "Effect": "Allow",
         "Resource": "${aws_kms_alias.lambda.arn}"
-        },
-        {
-        "Action": concat(var.lambda_policy, ["kms:ListAliases","kms:Decrypt"]),
-        "Effect": "Allow",
-        "Resource": "*"
-        },
-        {
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Effect": "Allow",
-        "Resource": "*"
-      }
+    },
+    {
+    "Action": [
+        "ec2:DescribeImages",
+        "ec2:DescribeSnapshotAttribute",
+        "ec2:DescribeSnapshots",
+        "ec2:DeleteSnapshot",
+        "ec2:DescribeImages",
+        "ec2:DescribeImageAttribute",
+        "ec2:DeregisterImage",
+        "ec2:DescribeInstances",
+        "kms:ListAliases",
+        "kms:Decrypt"
+    ],
+    "Effect": "Allow",
+    "Resource": "*"
+    },
+    {
+    "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+    ],
+    "Effect": "Allow",
+    "Resource": "*"
+    }
     ]
   })
 }
