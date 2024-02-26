04a91fc7fb63faf73048a7463de21394953b6723
lichadev <licha.pintos@gmail.com>
2024-02-25 22:51:40 -0300

    improving naming

--- policies.tf
+++ policies.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "delete_old_amis" {
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.id
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
